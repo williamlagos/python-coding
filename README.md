a5e8ab3e41e769d28b931c2ae1d60ceacbe518a0
William Oliveira de Lagos <william.lagos1@gmail.com>
2016-08-08 17:01:17 -0300

    Cleaned code on tests and added some description on README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # chess
-Simple chess engine written in Python
+Simple chess engine written in Python with standard library. Works with Python 3.x.
--- tests.py
+++ tests.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python
 #
 # Unique Combinations python unit test
-# The application that will be tested finds the unique combinations of X pieces on a M x N chessboard
-#
 # Author: William Oliveira de Lagos <william.lagos@outlook.com>
 #
 
-import unique
-import unittest
+""" Tests application finds the unique combinations of X pieces on a M x N chessboard """
 
-class UniqueApplicationTest(unittest.TestCase):
+import unittest
+import unique
+# import zones
+
+class ChessApplicationTest(unittest.TestCase):
+    """ Class of chess main application """
+
     def test_permutations(self):
-        unique.possible_ordered_sequences({'K':2,'Q':0,'R':1,'B':0,'N':0})
-        self.assertEqual('permutations','permutations')
+        """ Tests possible ordered sequences in permutation """
+        unique.possible_ordered_sequences({'K':2, 'Q':0, 'R':1, 'B':0, 'N':0})
+        self.assertEqual('permutations', 'permutations')
+
     def test_adjacencies(self):
-        self.assertEqual('adjacencies','adjacencies')
+        """ Tests adjacencies checking """
+        self.assertEqual('adjacencies', 'adjacencies')
+
     def test_insert(self):
-        self.assertEqual('insert','insert')
+        """ Tests chessboard piece insertion """
+        self.assertEqual('insert', 'insert')
 
 if __name__ == '__main__':
     unittest.main()
