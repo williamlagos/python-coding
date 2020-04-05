2cd280b3e16e4259357ad953b954edace4696d9d
William Oliveira de Lagos <william.lagos@icloud.com>
2020-04-05 20:38:23 -0300

    Added capi samples for python coding

---
+++ capi/module.h
@@ -0,0 +1,12 @@
+/*
+ * Python connection with C API, calling from C
+ * Copyright (C) 2009-2020 William Oliveira de Lagos <william.lagos@icloud.com>
+ */
+
+#ifndef MODULE_H
+#define MODULE_H
+
+#include <stdio.h>
+#include <Python.h>
+
+#endif /* MODULE_H */
