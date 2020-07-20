f3ffda8e53ddff51774b98f239f8ba522102666e
William Oliveira de Lagos <william.lagos@icloud.com>
2020-07-20 20:01:03 -0300

    Added valid default output

--- clouds/clouds.py
+++ clouds/clouds.py
@@ -8,16 +8,12 @@ import sys
 
 # Complete the jumpingOnClouds function below.
 def jumpingOnClouds(c):
+    return 0
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input())
-
     c = list(map(int, input().rstrip().split()))
-
     result = jumpingOnClouds(c)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
