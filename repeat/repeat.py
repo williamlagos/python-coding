4c98099801c3a08b07da248620e4e1a54a94c3b9
William Oliveira de Lagos <william.lagos@icloud.com>
2020-07-20 15:36:24 -0300

    Added base source tree for repeat problem

--- repeat/repeat.py
+++ repeat/repeat.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
-print('Hello World!')
+# print('Hello World!')
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the repeatedString function below.
+def repeatedString(s, n):
+    return ''
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    n = int(input())
+    result = repeatedString(s, n)
+    fptr.write(str(result) + '\n')
+    fptr.close()
