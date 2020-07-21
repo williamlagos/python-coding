92a25386db36e56c31c3505eb2b78ad2d6c44701
William Oliveira de Lagos <william.lagos@icloud.com>
2020-07-21 20:02:38 -0300

    Added constraints check on clouds algorithm

--- clouds/clouds.py
+++ clouds/clouds.py
@@ -8,6 +8,15 @@ import sys
 
 # Complete the jumpingOnClouds function below.
 def jumpingOnClouds(c):
+    clouds = 0
+    # Constraints check
+    clouds_quantity = len(c)
+    if clouds_quantity < 2 or clouds_quantity > 100:
+        return clouds
+    if c[0] == 0 and c[-1:] == 0:
+        return clouds
+    if c.count(0) + c.count(1) != clouds_quantity?
+        return clouds
     return 0
 
 if __name__ == '__main__':
