cf663e291d8843380ce6c53de375d6a0524bc50f
William Oliveira de Lagos <william.lagos@icloud.com>
2020-07-27 11:45:20 -0300

    Finished algorithm with simple constraint check

--- sorting/bubblesort.py
+++ sorting/bubblesort.py
@@ -8,11 +8,27 @@ import sys
 
 # Complete the countSwaps function below.
 def countSwaps(a):
-    pass
+    n = len(a)
+    # Constraints check
+    if n > 600:
+        return
+    # Bubble sort algorithm with swaps counting
+    last = n - 1
+    numSwaps = 0
+    arraySorted = False
+    while not arraySorted:
+        i = 0
+        while i < last:
+            arraySorted = True
+            if a[i] > a[i + 1]:
+                a[i], a[i + 1] = a[i + 1], a[i]
+                arraySorted = False
+                numSwaps += 1
+            i += 1
+    print("Array is sorted in %d swaps.\nFirst Element: %d\nLast Element: %d" % (numSwaps, a[0], a[last]))
+
 
 if __name__ == '__main__':
     n = int(input())
-
     a = list(map(int, input().rstrip().split()))
-
     countSwaps(a)
