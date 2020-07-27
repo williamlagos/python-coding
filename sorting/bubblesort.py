9c9511c0c1b97bdac6c31f10a20082aa3577860f
William Oliveira de Lagos <william.lagos@icloud.com>
2020-07-27 11:22:35 -0300

    Added new algorithm boilerplate code, starting sorting problems

---
+++ sorting/bubblesort.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countSwaps function below.
+def countSwaps(a):
+    pass
+
+if __name__ == '__main__':
+    n = int(input())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
