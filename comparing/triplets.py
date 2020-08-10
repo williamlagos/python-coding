57b82c4b749b70caf91a3065198589ff0f80e241
William Oliveira de Lagos <william.lagos@icloud.com>
2020-08-10 19:15:08 -0300

    Fixed some standards on coding

--- comparing/triplets.py
+++ comparing/triplets.py
@@ -27,14 +27,9 @@ def compareTriplets(a, b):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     a = list(map(int, input().rstrip().split()))
-
     b = list(map(int, input().rstrip().split()))
-
     result = compareTriplets(a, b)
-
     fptr.write(' '.join(map(str, result)))
     fptr.write('\n')
-
     fptr.close()
--- math/bigsum.py
+++ math/bigsum.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/env python3
 
 import math
 import os
@@ -8,6 +8,7 @@ import sys
 
 # Complete the aVeryBigSum function below.
 def aVeryBigSum(ar):
+    # Big integers on python are automatically assigned
     big_sum = 0
     for a in ar:
         big_sum += a
@@ -15,13 +16,8 @@ def aVeryBigSum(ar):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     ar_count = int(input())
-
     ar = list(map(int, input().rstrip().split()))
-
     result = aVeryBigSum(ar)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
