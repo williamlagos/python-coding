8314006b70cdba65dc635bb2cdb189d81c55ce70
William Oliveira de Lagos <william.lagos@icloud.com>
2020-08-10 19:24:28 -0300

    Tried the first solution for diagonal array

--- arrays/diagonal.py
+++ arrays/diagonal.py
@@ -15,7 +15,23 @@ import sys
 
 def diagonalDifference(arr):
     # Write your code here
-    pass
+    r = 0
+    n = len(arr)
+    i = 0
+    j = n - 1
+    i_sum = 0
+    j_sum = 0
+    # Checks if the 2D array have 
+    # different number of columns
+    if len(arr[0]) != n:
+        return
+    while r < n:
+        i_sum += arr[r][i]
+        j_sum += arr[r][j]
+        r += 1
+        i += 1
+        j -= 1
+    return abs(i_sum - j_sum)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
