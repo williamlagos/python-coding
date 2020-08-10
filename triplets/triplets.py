74ea3ce2af9f524d524afcc5ff0cce00d01609a4
William Oliveira de Lagos <william.lagos@icloud.com>
2020-08-10 18:51:16 -0300

    Added first source code for the triplets problem

---
+++ triplets/triplets.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
