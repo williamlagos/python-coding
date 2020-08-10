d1e34978200523b0cabf7bf3812b04b16fbc424b
William Oliveira de Lagos <william.lagos@icloud.com>
2020-08-10 19:06:54 -0300

    Finished triplets

--- triplets/triplets.py
+++ triplets/triplets.py
@@ -8,6 +8,22 @@ import sys
 
 # Complete the compareTriplets function below.
 def compareTriplets(a, b):
+    # Decompose the tuples for Alice and Bob
+    i = 0
+    a_points = 0
+    b_points = 0
+    
+    # Check for every position, in one loop
+    while i < 3:
+        if a[i] > b[i]:
+            a_points += 1
+        elif b[i] > a[i]:
+            b_points += 1
+        i += 1
+
+    # Return the sum of points for each other
+    return (a_points, b_points) 
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
