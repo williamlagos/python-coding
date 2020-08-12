5e608970b6072a26b7d6b0e76c0f02c511226085
William Oliveira de Lagos <william.lagos@icloud.com>
2020-08-12 02:52:20 -0300

    Added isolated cities constraint

--- graph/libraries.py
+++ graph/libraries.py
@@ -18,6 +18,7 @@ def roadsAndLibraries(n, c_lib, c_road, cities):
         for city in cities:
             # Check for empty city areas, then create the first one:
             if not c_areas:
+                # If there isn't any area, create the first one
                 c_areas.append(set(city))
             else:
                 i_areas = 0
@@ -25,15 +26,18 @@ def roadsAndLibraries(n, c_lib, c_road, cities):
                     # Check for any difference by intersection on all areas
                     points = set(city).difference(c_areas[i_areas])
                     if len(points) == 1:
+                        # If there is one intersection, add to its set
                         c_areas[i_areas] = c_areas[i_areas].union(points)
                         break
                     elif len(points) == 2:
+                        # If not, create another set 
                         c_areas.append(set(city))
                         break
                     else:
                         i_areas += 1
 
         unique_areas = []
+        # Make areas unique checking each other
         for area in c_areas:
             count = 0
             while count < len(c_areas):
@@ -41,13 +45,23 @@ def roadsAndLibraries(n, c_lib, c_road, cities):
                     unique_areas.append(area.union(c_areas[count]))
                 count += 1
         
+        # If there isn't any changes to make, reattribute the original country areas
         if not unique_areas:
             unique_areas = c_areas
-    
+
+        # Do the math with the minimum number of roads and libraries
         num_roads = 0
-        num_librs = len(unique_areas)
+        num_cities = 0
         for area in unique_areas:
-            num_roads += len(area)
+            num_cities += len(area)
+            num_roads += (len(area) - 1) * c_road
+
+        print(unique_areas)
+        print(c_areas)
+        # Check if there is any isolated city
+        isolated_cities = c_lib * (n - num_cities)
+        num_librs = c_lib * len(unique_areas) + isolated_cities
+
         min_cost = num_roads + num_librs
     
     return min_cost
