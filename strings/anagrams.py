c0da3c6cccc84bd8e388a484326856352f3f7b3a
William Oliveira de Lagos <william.lagos@icloud.com>
2020-08-15 14:40:11 -0300

    Started another problem and category on strings manipulation

---
+++ strings/anagrams.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the makeAnagram function below.
+def makeAnagram(a, b):
+    return ''
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = input()
+
+    b = input()
+
+    res = makeAnagram(a, b)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
