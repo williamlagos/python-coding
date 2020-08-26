fb32b8d89fd9de4610a0108daa9e462533bed2a3
William Oliveira de Lagos <william.lagos@icloud.com>
2020-08-26 19:01:04 -0300

    Added base code on minimum absolute

--- greedy/minimum.py
+++ greedy/minimum.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python3
-print('Hello World!')
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    return 0
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
