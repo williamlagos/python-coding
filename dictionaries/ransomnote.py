babf959e70ee7ed8d99d336c7b00d9551fcdfc77
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-01 19:12:05 -0300

    Added new sample for dictionaries problem solving challenge

---
+++ dictionaries/ransomnote.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the checkMagazine function below.
+def checkMagazine(magazine, note):
+    return 0
+
+if __name__ == '__main__':
+    mn = input().split()
+    m = int(mn[0])
+    n = int(mn[1])
+    magazine = input().rstrip().split()
+    note = input().rstrip().split()
+    checkMagazine(magazine, note)
