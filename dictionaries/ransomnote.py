be4f270afd0592de18a91981bd5d62b34eca9ed9
William Oliveira de Lagos <william.lagos@icloud.com>
2020-09-08 16:46:03 -0300

    Started to build a solution on checkMagazine function

--- dictionaries/ransomnote.py
+++ dictionaries/ransomnote.py
@@ -8,6 +8,10 @@ import sys
 
 # Complete the checkMagazine function below.
 def checkMagazine(magazine, note):
+    for word in magazine:
+        print(word)
+    for word in note:
+        print(word)
     return 0
 
 if __name__ == '__main__':
