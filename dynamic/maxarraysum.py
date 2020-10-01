81e31832594fbf63eecfa287bda63e4f4d4918e6
William Oliveira de Lagos <william.lagos@icloud.com>
2020-10-01 19:14:08 -0300

    Added some initial tests to dynamic programming alg

--- dynamic/maxarraysum.py
+++ dynamic/maxarraysum.py
@@ -6,14 +6,25 @@ import random
 import re
 import sys
 
+def powerSet(arr, itr):
+    print(arr)
+    if itr == 0:
+        return arr
+    else:
+        itr -= 1
+        powerSet(arr[1:], itr)
+
 # Complete the maxSubsetSum function below.
 def maxSubsetSum(arr):
     return ''
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    n = int(input())
-    arr = list(map(int, input().rstrip().split()))
-    res = maxSubsetSum(arr)
-    fptr.write(str(res) + '\n')
-    fptr.close()
+    powerSet([-2,1,3,-4,5],7)
+
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+#     n = int(input())
+#     arr = list(map(int, input().rstrip().split()))
+#     res = maxSubsetSum(arr)
+#     fptr.write(str(res) + '\n')
+#     fptr.close()
