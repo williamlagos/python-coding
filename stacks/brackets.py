6229edffaf30f4f95ff7ebb714f92587748a2ab8
William Oliveira de Lagos <william.lagos@icloud.com>
2020-10-02 13:20:50 -0300

    Finished brackets problem

--- stacks/brackets.py
+++ stacks/brackets.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/env python3
 
 import math
 import os
@@ -8,7 +8,25 @@ import sys
 
 # Complete the isBalanced function below.
 def isBalanced(s):
-    return ''
+    opn = []
+    enc = []
+    enclosing = False
+    for c in s:
+
+        if '{' == c or '[' == c or '(' == c:
+            opn.append(c)
+            enclosing = False
+        elif '}' == c or ']' == c or ')' == c: 
+            enc.append(c)
+            enclosing = True
+
+        if enclosing:
+            t = opn[-1:][0]
+            if t == '{' and c == '}' or t == '[' and c == ']' or t == '(' and c == ')':
+                opn.pop()
+                enc.pop()
+
+    return 'NO' if opn or enc else 'YES'
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
