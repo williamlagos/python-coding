943caf3ba5a7bd54d37d3d9614f29941afc35bb4
William Oliveira de Lagos <william.lagos@icloud.com>
2020-10-02 09:30:20 -0300

    Added first sample to develop in stacks categoy

---
+++ stacks/brackets.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the isBalanced function below.
+def isBalanced(s):
+    return ''
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    t = int(input())
+    for t_itr in range(t):
+        s = input()
+        result = isBalanced(s)
+        fptr.write(result + '\n')
+    fptr.close()
