54917065b464b5e484522c14660f4c16a81dbb82
William Oliveira de Lagos <william.lagos@icloud.com>
2020-10-08 11:27:36 -0300

    Finished lca

--- trees/lca.py
+++ trees/lca.py
@@ -46,9 +46,55 @@ class Node:
        // this is a node of the tree , which contains info as data, left , right
 '''
 
-def lca(root, v1, v2):
-  #Enter your code here
-  return root
+
+# Finds the path from root node to given root of the tree. 
+# Stores the path in a list path[], returns true if path  
+# exists otherwise false 
+def findPath(root, path, k): 
+  
+    # Baes Case 
+    if root is None: 
+        return False
+  
+    # Store this node is path vector. The node will be 
+    # removed if not in path from root to k 
+    path.append(root) 
+  
+    # See if the k is same as root's key 
+    if root.info == k : 
+        return True
+  
+    # Check if k is found in left or right sub-tree 
+    if ((root.left != None and findPath(root.left, path, k)) or
+            (root.right!= None and findPath(root.right, path, k))): 
+        return True 
+  
+    # If not present in subtree rooted with root, remove 
+    # root from path and return False 
+       
+    path.pop() 
+    return False
+  
+# Returns LCA if node n1 , n2 are present in the given 
+# binary tre otherwise return -1 
+def lca(root, n1, n2): 
+  
+    # To store paths to n1 and n2 fromthe root 
+    path1 = [] 
+    path2 = [] 
+  
+    # Find paths from root to n1 and root to n2. 
+    # If either n1 or n2 is not present , return -1  
+    if (not findPath(root, path1, n1) or not findPath(root, path2, n2)): 
+        return -1 
+  
+    # Compare the paths to get the first different value 
+    i = 0 
+    while(i < len(path1) and i < len(path2)): 
+        if path1[i] != path2[i]: 
+            break
+        i += 1
+    return path1[i-1] 
 
 tree = BinarySearchTree()
 t = int(input())
