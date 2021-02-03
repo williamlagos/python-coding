bb0bd24aaab508a04803093c411b64efa784881f
William Oliveira de Lagos <william.lagos@icloud.com>
2021-02-03 18:11:08 +0000

    Added new skeleton code for new problem solving

---
+++ dictionaries/twostrings.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the twoStrings function below.
+def twoStrings(s1, s2):
+    pass
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    q = int(input())
+    for q_itr in range(q):
+        s1 = input()
+        s2 = input()
+        result = twoStrings(s1, s2)
+        fptr.write(result + '\n')
+    fptr.close()
