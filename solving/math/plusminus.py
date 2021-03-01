4040865991cfadabdbdd4e92381496e72c8302bc
William Oliveira de Lagos <william.lagos@icloud.com>
2021-03-01 17:26:28 +0000

    Added base for integers problem

---
+++ solving/math/plusminus.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the plusMinus function below.
+def plusMinus(arr):
+    return []
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
