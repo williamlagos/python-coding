c5498a00a2a64dea2d85f37cd34551c75f8e1ec5
William Oliveira de Lagos <william.lagos@icloud.com>
2021-03-01 17:36:44 +0000

    Finished  solving plus minus

--- solving/math/plusminus.py
+++ solving/math/plusminus.py
@@ -8,11 +8,19 @@ import sys
 
 # Complete the plusMinus function below.
 def plusMinus(arr):
-    return []
+    r = [0, 0, 0]
+    l = len(arr)
+    # Iterate over array
+    for i in arr:
+        # Check if its lesser, equal, or bigger than zero
+        if i > 0: r[0] += 1
+        elif i < 0: r[1] += 1
+        elif i == 0: r[2] += 1
+    # Print the three ratios in order
+    for i in r:
+        print(i/l)
 
 if __name__ == '__main__':
     n = int(input())
-
     arr = list(map(int, input().rstrip().split()))
-
     plusMinus(arr)
