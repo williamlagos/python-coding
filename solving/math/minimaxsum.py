6982677b869fb876812b8f75ccb50b86c61caf68
William Oliveira de Lagos <william.lagos@icloud.com>
2021-03-07 18:19:30 +0000

    Added structure for solving mini max sum problem

---
+++ solving/math/minimaxsum.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    pass
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
