f4d999c5aeafc1cd35de07881c498b56c8de37b0
William Oliveira de Lagos <william.lagos@icloud.com>
2021-03-08 18:36:20 +0000

    Finished minimaxsum problem

--- solving/math/minimaxsum.py
+++ solving/math/minimaxsum.py
@@ -5,12 +5,15 @@ import os
 import random
 import re
 import sys
+import itertools
 
 # Complete the miniMaxSum function below.
 def miniMaxSum(arr):
-    pass
+    sums = []
+    for subset in itertools.combinations(arr, 4):
+        sums.append(sum(subset))
+    print('%d %d' % (min(sums), max(sums)))
 
 if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
-
     miniMaxSum(arr)
