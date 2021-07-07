f01238a8bd09dda8f3a959a7f9dc3c3729b1ab90
William Oliveira de Lagos <william.lagos@icloud.com>
2021-07-07 15:45:07 +0100

    Added first source code for the Hourglass Sum problem

---
+++ solving/lists/hourglass.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Problem link: https://www.hackerrank.com/challenges/2d-array/problem
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    # Write your code here
+    return ''
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
