7470479e352bf6fa28215e745af8c42dc20d7a1f
William Oliveira de Lagos <william.lagos@icloud.com>
2021-07-08 17:40:08 +0100

    Added hourglass matrix python script

--- solving/lists/hourglass.py
+++ solving/lists/hourglass.py
@@ -16,18 +16,20 @@ import sys
 
 def hourglassSum(arr):
     # Write your code here
-    return ''
+    m = len(arr)
+    sums = []
+    for x in range(m - 2):
+        for y in range(m - 2):
+            sums.append(sum([arr[x][y],      arr[x][y + 1],      arr[x][y + 2],
+                                             arr[x + 1][y + 1],
+                             arr[x + 2][y],  arr[x + 2][y + 1],  arr[x + 2][y + 2]]))
+    return max(sums)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     arr = []
-
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
-
     result = hourglassSum(arr)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
